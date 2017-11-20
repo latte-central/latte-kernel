052c74a234afcc870f25b34bd48016b12bb8ac88
fredokun <frederic.peschanski@lip6.fr>
2017-11-20 15:17:47 +0100

    version bump (lost ?)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject latte-kernel "0.99.9-SNAPSHOT"
+(defproject latte-kernel "0.100.0-SNAPSHOT"
   :description "The (very) small kernel of the LaTTe proof assistant"
   :url "https://github.com/latte-central/latte-kernel.git"
   :license {:name "MIT Licence"
