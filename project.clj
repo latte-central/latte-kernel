1a84288f9318d9fa157e5ce9025749f4f329a7f5
fredokun <frederic.peschanski@lip6.fr>
2019-09-25 15:20:35 +0200

    version sync

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject latte-kernel "1.0b5-SNAPSHOT"
+(defproject latte-kernel "1.0b6-SNAPSHOT"
   :description "The (very) small kernel of the LaTTe proof assistant"
   :url "https://github.com/latte-central/latte-kernel.git"
   :license {:name "MIT Licence"
